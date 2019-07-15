9a688c3bff82ec2822e9757c2df977843c415da6
wuxidalao <yatouxixi520@qq.com>
2019-07-15 17:27:50 +0800

    1

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="header">
-    <div class="header-left">1</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">3</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe676;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe67b;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市<span class="iconfont arrow-icon">&#xe6aa;</span>
+    </div>
   </div>
 </template>
 
@@ -13,29 +20,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+// 1rem = 1/2*100
+@import '~styles/varibles.styl'
   .header
-    display flex
-    height .86rem
-    background: #00bcd4
+    display: flex
+    align-items: center
+    height: .86rem
+    background: $bjColor
     color: #fff
-
     .header-left
-      width : .64rem
+      width: .64rem
       float: left
-
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
       height: .64rem
       line-height: .64rem
       margin-left: .2rem
-      margin-top: .12rem
       padding-left: .2rem
       background: #ffffff
       border-radius: .1rem
       color: #cccccc
-
     .header-right
       width: 1.24rem
       float: right
       text-align: center
+      .arrow-icon
+        font-size: .24rem
+        margin-left: .04rem
 </style>
