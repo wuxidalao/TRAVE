0ba71c960b60b7a7c2b8e57186207a026ec52da1
wuxidalao <yatouxixi520@qq.com>
2019-07-15 15:26:19 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastClick'// 移动端300毫秒点击延迟问题解决
+import './assets/styles/reset.css'// 全局页面样式
+import './assets/styles/border.css'// 解决移动端1px边框问题
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
